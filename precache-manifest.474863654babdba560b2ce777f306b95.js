7a2b65ab17deccb39c7ccfdd2481aa9530c296e1
Yaroslav Shilov <workyarik@gmail.com>
2020-08-28 16:23:18 +0300

    Updates

---
+++ precache-manifest.474863654babdba560b2ce777f306b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f48c520b62c877acb8b91c8930da38b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "716bf612858167b1a7e4",
+    "url": "/static/css/main.f584227e.chunk.css"
+  },
+  {
+    "revision": "ae67e6fa3aad9f2d2cac",
+    "url": "/static/js/2.add2015a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.add2015a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716bf612858167b1a7e4",
+    "url": "/static/js/main.2887c38c.chunk.js"
+  },
+  {
+    "revision": "20e603b48db7056c0ae9",
+    "url": "/static/js/runtime-main.ee0fb51b.js"
+  }
+]);
